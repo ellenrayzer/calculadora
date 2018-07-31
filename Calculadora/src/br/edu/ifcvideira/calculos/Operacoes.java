7ab51c7a56820647441c84dd357582ca0ae385d0
ellenrayzer <ellenrayzer@hotmail.com>
2018-07-31 08:24:10 -0300

    Criado projeto e funções iniciais

---
+++ Calculadora/src/br/edu/ifcvideira/calculos/Operacoes.java
@@ -0,0 +1,26 @@
+package br.edu.ifcvideira.calculos;
+
+import javax.swing.*;
+
+public class Operacoes {
+   public static void main(String[] args) {
+      
+      String numeroA = JOptionPane.showInputDialog(null, "Digite um número:");
+      String numeroB = JOptionPane.showInputDialog(null, "Digite o segundo número:");
+      
+      double a = Double.parseDouble(numeroA);
+      double b = Double.parseDouble(numeroB);
+ 
+      double soma = a + b;
+      double sub = a - b;
+      double mult = a * b;
+      double div = a / b;
+
+      JOptionPane.showMessageDialog(null,
+            "Soma: " + soma + "\n" +
+            "Subtração: " + sub + "\n" +
+            "Multiplicação: " + mult + "\n" +
+            "Divisão: " + div
+      );
+   }
+}
